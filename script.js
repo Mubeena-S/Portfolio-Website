a8d0cda95ac94e2958991b98cd7dcf8d8b8da623
Mubeena-S <savalagimubeena@gmail.com>
2024-12-20 13:50:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example JS to handle form submission (you can add more functionality as needed)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert("Message Sent! We will get back to you soon.");
+});
