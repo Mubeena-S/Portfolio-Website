1e93aa0c8da20514d3aad0528118b73d6d72d752
Mubeena-S <savalagimubeena@gmail.com>
2025-01-02 14:37:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function changeSlide(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            const slides = document.querySelectorAll(".slides");
+            const dots = document.querySelectorAll(".dot");
+            if (n > slides.length) slideIndex = 1;
+            if (n < 1) slideIndex = slides.length;
+            slides.forEach(slide => slide.style.display = "none");
+            dots.forEach(dot => dot.classList.remove("active"));
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].classList.add("active");
+        }
 // Toggle navbar for mobile view
 const menuToggle = document.querySelector('.menu-toggle');
 const navbar = document.querySelector('.navbar ul');
